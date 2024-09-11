0131fd72cb9b1e454a0a0ed8e5f1180cb6ff0c46
Sionparadox <sion9999@naver.com>
2024-09-11 12:56:40 +0900

    ex5 complete

--- FEBasic/박시온/ex5.js
+++ FEBasic/박시온/ex5.js
@@ -1,3 +1,13 @@
 module.exports = {
-  searchByKoreanInitialSound: (data, firstSounds) => {},
+  searchByKoreanInitialSound: (data, firstSounds) => {
+    const INITLETTER = 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ';
+    const regArr = firstSounds.split('').map((a) => {
+      if (!isNaN(a)) return a.charCodeAt() - 48;
+      const startIdx = INITLETTER.indexOf(a) * 588 + 44032;
+      const endLet = String.fromCharCode(startIdx + 587);
+      return `[${a}${String.fromCharCode(startIdx)}-${endLet}]`;
+    });
+    const check = new RegExp(`${regArr.join('')}`, 'g');
+    return data.filter((a) => a.match(check));
+  },
 };
