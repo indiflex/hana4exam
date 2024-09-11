be0d70f0283649ff609277826a80d2f8ec910e3c
Sionparadox <sion9999@naver.com>
2024-09-11 12:55:46 +0900

    ex3 complete

--- FEBasic/박시온/ex3.js
+++ FEBasic/박시온/ex3.js
@@ -1,3 +1,11 @@
 Array.prototype.sortBy = function (sortProp = '') {
+  sortProp
+    .split(',')
+    .map((kd) => {
+      const temp = kd.split(':');
+      return [temp[0], !temp[1] || temp[1] === 'asc' ? 1 : -1];
+    })
+    .reverse()
+    .forEach(([k, d]) => this.sort((a, b) => (a[k] > b[k] ? d : -d)));
   return this;
 };
