f9a961078b2f48f0f30f77ae7a705890702ab5e2
Sionparadox <sion9999@naver.com>
2024-09-11 12:56:16 +0900

    ex4 complete

--- FEBasic/박시온/ex4.js
+++ FEBasic/박시온/ex4.js
@@ -1,3 +1,22 @@
-function deepCopy(obj) {}
+function deepCopy(obj) {
+  if (!obj || !(typeof obj === 'object')) return obj;
+
+  let ret;
+  if (obj instanceof Map) {
+    ret = new Map();
+    for (const [k, v] of obj) ret.set(k, deepCopy(v));
+  } else if (obj instanceof Set) {
+    ret = new Set();
+    for (const v of obj) ret.add(deepCopy(v));
+  } else if (obj instanceof WeakMap || obj instanceof WeakSet) {
+    ret = obj;
+  } else {
+    ret = Array.isArray(obj) ? [] : {};
+    for (const k of Reflect.ownKeys(obj)) {
+      ret[k] = deepCopy(obj[k]);
+    }
+  }
+  return ret;
+}
 
 module.exports = { deepCopy };
